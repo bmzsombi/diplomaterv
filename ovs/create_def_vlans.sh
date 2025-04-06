4b3481f6da049574f5a26f04ccf695a14016f2eb
bmzsombi <bmzsombi@gmail.com>
2025-04-06 16:47:37 +0200

    faucet config update

--- ovs/create_def_vlans.sh
+++ ovs/create_def_vlans.sh
@@ -22,13 +22,16 @@ create_ns () {
 create_ns host1 10.0.0.1/24
 create_ns host2 10.0.1.2/24
 create_ns frr 192.168.10.70/24
-as_ns frr sudo systemctl start frr
+as_ns frr sudo systemctl restart frr
 sudo ovs-vsctl add-br br0 \
 -- set bridge br0 other-config:datapath-id=0000000000000001 \
 -- set bridge br0 other-config:disable-in-band=true \
 -- set bridge br0 fail_mode=secure \
 -- add-port br0 veth-host1 -- set interface veth-host1 ofport_request=1 \
 -- add-port br0 veth-host2 -- set interface veth-host2 ofport_request=2 \
+-- add-port br0 veth-frr -- set interface veth-frr ofport_request=3 \
 -- set-controller br0 tcp:192.168.10.65:6653
 
-# -- add-port br0 veth-frr -- set interface veth-frr ofport_request=3 \
+as_ns host1 ip route add default via 10.0.0.254 dev veth0
+as_ns host2 ip route add default via 10.0.1.254 dev veth0
+as_ns frr ip route add default via 192.168.10.127 dev veth0
