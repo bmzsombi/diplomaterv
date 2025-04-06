b27e866b6e899ba53b845934fbb2e42084545946
bmzsombi <bmzsombi@gmail.com>
2025-04-06 16:22:09 +0200

    faucet config update

--- ovs/create_def_vlans.sh
+++ ovs/create_def_vlans.sh
@@ -1,11 +1,33 @@
 #!/bin/bash
 
+as_ns () {
+    NAME=$1
+    NETNS=faucet-${NAME}
+    shift
+    sudo ip netns exec ${NETNS} $@
+}
+
+create_ns () {
+    NAME=$1
+    IP=$2
+    NETNS=faucet-${NAME}
+    sudo ip netns add ${NETNS}
+    sudo ip link add dev veth-${NAME} type veth peer name veth0 netns ${NETNS}
+    sudo ip link set dev veth-${NAME} up
+    as_ns ${NAME} ip link set dev lo up
+    [ -n "${IP}" ] && as_ns ${NAME} ip addr add dev veth0 ${IP}
+    as_ns ${NAME} ip link set dev veth0 up
+}
+
 create_ns host1 10.0.0.1/24
 create_ns host2 10.0.1.2/24
+create_ns frr 192.168.10.70/24
+as_ns frr sudo systemctl start frr
 sudo ovs-vsctl add-br br0 \
 -- set bridge br0 other-config:datapath-id=0000000000000001 \
 -- set bridge br0 other-config:disable-in-band=true \
 -- set bridge br0 fail_mode=secure \
 -- add-port br0 veth-host1 -- set interface veth-host1 ofport_request=1 \
 -- add-port br0 veth-host2 -- set interface veth-host2 ofport_request=2 \
+-- add-port br0 veth-frr -- set interface veth-frr ofport_request=3 \
 -- set-controller br0 tcp:192.168.10.65:6653
