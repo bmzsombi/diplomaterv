bd4e27f7e90837a292e78c209e79a603245696cf
bmzsombi <bmzsombi@sch.bme.hu>
2025-05-08 20:00:02 +0200

    home migr

--- ovs/create_br1.sh
+++ ovs/create_br1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo ovs-vsctl add-br br1 \
--- set bridge br1 other-config:datapath-id=0000000000000001 \
+-- set bridge br1 other-config:datapath-id=0000000000000002 \
 -- set bridge br1 other-config:disable-in-band=true \
 -- set bridge br1 fail_mode=secure \
--- set-controller br1 tcp:192.168.10.65:6653
+-- set-controller br1 tcp:192.168.10.1:6653
